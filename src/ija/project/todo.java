a75a3aa5211137a7475af78710a7c4544eecae22
lakluk <lukas.fuis@seznam.cz>
2022-04-03 21:56:49 +0200

    Basic rozložení basic tlačítek

---
+++ src/ija/project/todo.java
@@ -0,0 +1,11 @@
+package ija.project;
+
+public class todo {
+    //TODO Tlačítko add přidá objekt
+    //TODO Tlačítko delete smaže objekt
+    //TODO Objektu se dá přidat atribut
+    //TODO Objektem se dá hýbat
+    //TODO Tlačítko connect spojí objekty
+    //TODO Tlačítko save uloží diagram
+    //TODO Tlačítko open otevře soubor s diagramem a zobrazí jej
+}
